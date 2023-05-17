00e77e5e2e97b2d65b1a731f560b89bbdbaa0c81
Isaac Wang <isaac.wang.us@gmail.com>
2023-05-17 16:23:21 -0400

    refine zero-T renormalization: add scalar contribution to the effective potential

---
+++ phase_transition/ALP.py
@@ -0,0 +1,12 @@
+"""
+Title: ALP.py
+Author: Isaac R. Wang
+This code defines the effective potential of the model in paper.
+Python package "cosmoTransitions" is used.
+Convention and analytical expressions can be found in the paper.
+"""
+
+import numpy as np
+import numpy.linalg as la
+from cosmoTransitions import generic_potential
+
