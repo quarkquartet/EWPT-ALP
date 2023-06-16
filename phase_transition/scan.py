cef67bc01e645121b84c34c7e853d5490aacf95b
Isaac Wang <isaac.wang.us@gmail.com>
2023-06-16 16:36:51 -0400

    updated scanning algorithm. Better input and output format.

--- phase_transition/scan.py
+++ phase_transition/scan.py
@@ -2,31 +2,11 @@ import csv
 import numpy as np
 from ALP import model_ALP as model
 import sys
-import os
-
-mass_flag = sys.argv[1]
-c_value = sys.argv[2]
-beta_value = sys.argv[3]
-
-param_file_dir = "../model_setup/output/"
-output_file_dir = "./output/"
 
 
-file_name = (
-    "c" + str(int(c_value)) + "_beta_pi" + str(int(beta_value)) + "_" + str(mass_flag)
-)
-param_file_name = file_name + "_param.csv"
-output_file_name = file_name + "_out.csv"
-param_file = os.path.join(param_file_dir, param_file_name)
-output_file = os.path.join(output_file_dir, output_file_name)
+param_file = sys.argv[1]
+output_file = sys.argv[2]
 
-print(
-    "Running for f = "
-    + str(int(c_value))
-    + " f_c and beta = "
-    + str(int(beta_value))
-    + "."
-)
 
 dataset = []
 with open(param_file) as csv_file:
@@ -68,6 +48,29 @@ with open(output_file, "w") as f:
         strength_1d = md.strength_Tn_1d()
         strength = md.strength_Tn()
         strength_Tc = md.strength_Tc
-        data_writer.writerow([mS, sintheta, strength, strength_1d, strength_Tc])
+        Tc = md.Tc
+        Tnuc = md.Tn
+        Tnuc_1d = md.Tn1d
+        beta_H = md.beta_over_H()
+        beta_H_1d = md.beta_over_H_1d()
+        data_writer.writerow(
+            [
+                mS,
+                sintheta,
+                lh,
+                Ap,
+                muhsq,
+                muSsq,
+                f,
+                strength,
+                strength_1d,
+                strength_Tc,
+                Tc,
+                Tnuc,
+                Tnuc_1d,
+                beta_H,
+                beta_H_1d,
+            ]
+        )
         print("mS = " + str(mS) + ", sin theta = " + str(sintheta) + " scanning done.")
         print("PT strength: " + str(strength))
